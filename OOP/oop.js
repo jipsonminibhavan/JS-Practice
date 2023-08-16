a08c960cb1cb49a738cb5aa24289431765d15d2f
Jipson Minibhavan <jipson.minibhavan@googlemail.com>
2023-08-16 15:47:28 +0200

    update

--- OOP/oop.js
+++ OOP/oop.js
@@ -209,3 +209,37 @@ const Jin = {
 };
 
 console.log(Jin.age);
+
+//ES6
+
+class CarCL {
+  constructor(make, spped) {
+    this.make = make;
+    this.speed = spped;
+  }
+
+  accerlate() {
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+  }
+
+  brake() {
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+  }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+const ford = new CarCL("Ford", 120);
+console.log(ford.speedUS);
+ford.accerlate();
+ford.brake();
+ford.speedUS = 50;
+console.log(ford);
